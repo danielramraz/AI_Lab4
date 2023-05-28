c22f445be13eb4ec7a736774c0a08f7adb20bc43
danielramraz <79479383+danielramraz@users.noreply.github.com>
2023-05-28 16:09:47 +0300

    wip

--- CoEvolution.py
+++ CoEvolution.py
@@ -7,34 +7,38 @@ import SortingNetwork
 # ----------- Python Package -----------
 import time
 import threading
-
-
 # ----------- Consts ----------
-
 setting_vector = [6]
+
 
 class CoEvolution:
     data: Data
     sorting_networks: SortingNetworkPopulation
     challengers: UnsolvedSoringPopulation
-    testing: TestHub
+    # testing: TestHub
     solution: SortingNetwork
 
     def __init__(self) -> None:
         self.data = Data(setting_vector)
         self.challengers = UnsolvedSoringPopulation(self.data)
-        
+        # self.testing = TestHub()
         return
 
     def solve_sorting_network_problem(self) -> None:
         for generation in range(self.data.max_generations):
-            self.testing.parasites = self.challengers.get_parasites()
-            self.testing.sorting_networks = self.sorting_networks.get_sorting_networks()
-            self.testing.run_tests()
-            self.sorting_networks.tests_results = self.testing.get_sorting_network_tests_results()
-            self.challengers.tests_results = self.testing.get_parasites_tests_results()
+            parasites = self.challengers.get_parasites()
+            sorting_networks = self.sorting_networks.get_sorting_networks()
+            
+            sorting_network_tests_results, parasites_tests_results = self.run_tests(parasites, sorting_networks)
+            
+            self.sorting_networks.tests_results = sorting_network_tests_results
+            self.challengers.tests_results = parasites_tests_results
         return
     
     def print_solution_as_network(self) -> None:
         
+        return
+
+    def run_tests(self, parasites, sorting_networks) -> None:
+    
         return
