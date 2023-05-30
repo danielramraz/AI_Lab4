45377e3488a70511fac58525bdcfd2ade91832e9
MichalMarom <100923568+MichalMarom@users.noreply.github.com>
2023-05-30 18:57:53 +0300

    build sorting network
    נשאר לבצע :
    -מיון עם הפרזיטים ובהתאם לסדר את המודל
    - להוסיף דרך למדוד גוון גנטי+ מספר אלילים
    - להוסיף דרך התמודדות עם חוסר גיוון וציקליות

--- CoEvolution.py
+++ CoEvolution.py
@@ -1,17 +1,15 @@
 # ----------- Project Files -----------
 from Data import Data
-import SortingNetworkPopulation
+from SortingNetworkPopulation import SortingNetworkPopulation
 import TestHub
 from UnsolvedSoringPopulation import UnsolvedSoringPopulation
 import SortingNetwork
 # ----------- Python Package -----------
 import time
 import threading
-
-
 # ----------- Consts ----------
+setting_vector = [16]
 
-setting_vector = [6]
 
 class CoEvolution:
     data: Data
@@ -23,16 +21,18 @@ class CoEvolution:
     def __init__(self) -> None:
         self.data = Data(setting_vector)
         self.challengers = UnsolvedSoringPopulation(self.data)
+        self.sorting_networks = SortingNetworkPopulation(self.data)
         
         return
 
     def solve_sorting_network_problem(self) -> None:
         for generation in range(self.data.max_generations):
-            self.testing.parasites = self.challengers.get_parasites()
-            self.testing.sorting_networks = self.sorting_networks.get_sorting_networks()
-            self.testing.run_tests()
-            self.sorting_networks.tests_results = self.testing.get_sorting_network_tests_results()
-            self.challengers.tests_results = self.testing.get_parasites_tests_results()
+            # self.testing.parasites = self.challengers.get_parasites()
+            self.sorting_networks.genetic_algorithm()
+            # self.testing.sorting_networks = self.sorting_networks.get_sorting_networks()
+            # self.testing.run_tests()
+            # self.sorting_networks.tests_results = self.testing.get_sorting_network_tests_results()
+            # self.challengers.tests_results = self.testing.get_parasites_tests_results()
         return
     
     def print_solution_as_network(self) -> None:
--- Data.py
+++ Data.py
@@ -1,3 +1,5 @@
+# ----------- File For Genetic Algorithm -----------
+import SmartInit
 inputs_text_sorting_list_size = "enter the size of the vectors: \n"
 
 
@@ -5,8 +7,10 @@ class Data:
     sorting_list_size: int
     population_size: int
     max_generations: int
+    smart_init_vector: list
+    num_comparators_init_vector: int
 
-    def __init__(self, setting_vector = None):
+    def __init__(self, setting_vector=None):
         if setting_vector:
             self.init_with_settings(setting_vector)
         else:
@@ -17,21 +21,27 @@ class Data:
 
     def init_with_user_input(self):
         self.sorting_list_size = int(input(inputs_text_sorting_list_size))
+        self.init_smart_vector()
         return
 
     def init_with_settings(self, setting_vector):
-        # self.num_genes = setting_vector[1]
-        self.size_vector = setting_vector
-        return
-
-    def _init_consts(self):
-        self.pop_size = 100
-        self.max_generations = 10
-        self.cross_operator = 0
         self.sorting_list_size = setting_vector[0]
+        self.init_smart_vector()
         return
 
     def _init_consts(self):
         self.population_size = 100
         self.max_generations = 100
         return
+
+    def init_smart_vector(self):
+        if self.sorting_list_size == 16:
+            # self.smart_init_vector = SmartInit.smart_vector_16().copy()
+            self.num_comparators_init_vector = SmartInit.num_comparators_init_vector_16
+        elif self.sorting_list_size == 8:
+            # self.smart_init_vector = SmartInit.smart_vector_8().copy()
+            self.num_comparators_init_vector = SmartInit.num_comparators_init_vector_8
+
+        return
+
+
