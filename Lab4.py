a11c1f2ebe4c0556cec4cedd98c489e5f719cdeb
danielramraz <79479383+danielramraz@users.noreply.github.com>
2023-09-05 20:00:33 +0300

    create EXE file

--- Lab4.py
+++ Lab4.py
@@ -9,6 +9,7 @@ def main():
     co_evolution = CoEvolution.CoEvolution()
     co_evolution.solve_sorting_network_problem()
     co_evolution.print_solution_as_network()
+    input("Press Enter to exit...")
 
 if __name__ == "__main__":
     main()
